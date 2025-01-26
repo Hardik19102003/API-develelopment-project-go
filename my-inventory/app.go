7000550d114e6bca156929933901b592a3ae4c81
Hardik19102003 <202103032@daiict.ac.in>
2025-01-26 23:30:34 +0530

    my-inventory project created

---
+++ my-inventory/app.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (app *App) Initialize() error {
+	connectionString := fmt.Sprintf("%v:%v@tcp(127.0.0.1:3306)/%v", DbUser, DbPassword, DbName)
+	var err error
+	app.DB, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		return err
+	}
+
+	app.Router = mux.NewRouter().StrictSlash(true)
+	return nil
+}
+
+func (app *App) Run(address string) {
+	log.Fatal(http.ListenAndServe(address, app.Router))
+}
+
+func sendResponse(w http.ResponseWriter, statusCode int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(response)
+}
+
+func SendError(w http.ResponseWriter, statusCode int, err string) {
+	error_message := map[string]string{"error": err}
+	sendResponse(w, statusCode, error_message)
+}
+
+func (app *App) getProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := getProducts(app.DB)
+	if err != nil {
+		SendError(w, http.StatusInternalServerError, err)
+		return
+	}
+	sendResponse(w, http.StatusOK, products)
+}
+
+func (app *App) handleRoutes() {
+	app.Router.HandleFunc("/products", getProducts).Methods("GET")
+}
